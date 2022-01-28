161968c216c21526371ba975c65506c997f14743
Dr.C <dclinkenbeard@csumb.edu>
2022-01-27 16:48:09 -0800

    Started on the landing page. Haven't done the layout yet.

---
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/landingPageActivity.java
@@ -0,0 +1,14 @@
+package com.daclink.drew.sp22.cst438_project01_starter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class landingPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
